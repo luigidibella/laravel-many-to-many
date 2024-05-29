7b9114e26751249fd1a73060dc8e19b63423fca0
luigidibella <lykos.omega@gmail.com>
2024-05-29 16:08:20 +0200

    [edit] guest.home

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
-<h1>Home sito pubblico</h1>
+    <div class="d-flex-center flex-column py-5 bg-dark vh-100">
+        <h1 class="mb-5 text-white">Home sito pubblico</h1>
+
+        <img src="/img/astronauta.jpg" alt="astronauta" style="width: 500px; border-radius: 50%; border: 3px dashed white;">
+
+        <h1 class="mt-5 text-white">Benvenuto</h1>
+    </div>
 
 @endsection
